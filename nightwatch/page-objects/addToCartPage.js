297ff2ffa07dc7f94d838ce6ffdb978222426d3f
Nidhi Yadav <152848941+NidhiYadav726@users.noreply.github.com>
2024-04-02 12:29:32 +0530

    Update addToCartPage.js

--- nightwatch/page-objects/addToCartPage.js
+++ nightwatch/page-objects/addToCartPage.js
@@ -16,8 +16,8 @@ module.exports =  {
                 browser
                     .waitForElementPresent('#item_4_title_link > div ', 5000, 'Product link present')
                     .click('#item_4_title_link > div')
-                     .waitForElementPresent('.btn_primary.btn_inventory'', 5000, 'Add to Cart button present')
-                    .click('.btn_primary.btn_inventory'');
+                     .waitForElementPresent('.btn_primary.btn_inventory', 5000, 'Add to Cart button present')
+                    .click('.btn_primary.btn_inventory');
                 return this;
             },
         },
